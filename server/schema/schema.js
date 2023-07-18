ab261cd622ad9d56d26b628a2774f5b04eea0538
jintodas <jintolnuis@gmail.com>
2023-07-18 10:59:45 +0200

    update schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -102,6 +102,13 @@ const mutation = new GraphQLObjectType({
         id: { type: GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
+        Project.find({clientId: args.id}).then(
+          (projects)=> {
+            projects.forEach((project) => {
+              project.deleteOne();
+            });
+          }
+        );
         return Client.findByIdAndRemove(args.id);
       },
     },
@@ -144,10 +151,44 @@ const mutation = new GraphQLObjectType({
       },
       resolve(parent, args){
         return Project.findByIdAndRemove(args.id);
-      }
-     }
+      },
+     },
+  // Update a project
+  updateProject: {
+    type: ProjectType,
+    args: {
+      id: { type: GraphQLNonNull(GraphQLID) },
+      name: { type: GraphQLString },
+      description: { type: GraphQLString },
+      status: {
+        type: new GraphQLEnumType({
+          name: 'ProjectStatusUpdate',
+          values: {
+            new: { value: 'Not Started' },
+            progress: { value: 'In Progress' },
+            completed: { value: 'Completed' },
+          },
+        }),
+      },
+    },
+    resolve(parent, args) {
+      return Project.findByIdAndUpdate(
+        args.id,
+        {
+          $set: {
+            name: args.name,
+            description: args.description,
+            status: args.status,
+          },
+        },
+        { new: true }
+      );
+    },
   },
+},
 });
+
+
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
